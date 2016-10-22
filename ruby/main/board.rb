c65c7423ecfa95f51c707ca76631d7786a260907
Steven Abrams <steven.abrams86@gmail.com>
2016-10-22 17:18:50 -0600

    almost working

--- ruby/main/board.rb
+++ ruby/main/board.rb
@@ -17,4 +17,36 @@ class Board
     end
   end
 
+  def calculate(cells)
+    birthed = Array.new
+    survived = Array.new
+    (0..height-1).each do |y|
+      (0..length-1).each do |x|
+        neighbors = calculateNeighbors(cells, x,y)
+        if(cells.include?([x,y]))
+          survived.push([x,y]) if neighbors == 2 || neighbors == 3
+        else
+          birthed.push([x,y]) if neighbors == 3
+        end
+      end
+    end
+    return birthed + survived
+  end
+
+  def calculateNeighbors(cells, x,y)
+    neighbors = 0
+    # top row
+    neighbors = neighbors + 1 if cells.include([x-1, y+1])
+    neighbors = neighbors + 1 if cells.include([x, y+1])
+    neighbors = neighbors + 1 if cells.include([x+1, y+1])
+    # middle row
+    neighbors = neighbors + 1 if cells.include([x, y-1])
+    neighbors = neighbors + 1 if cells.include([x, y+1])
+    # bottom row
+    neighbors = neighbors + 1 if cells.include([x-1, y-1])
+    neighbors = neighbors + 1 if cells.include([x, y-1])
+    neighbors = neighbors + 1 if cells.include([x+1, y-1])
+    return neighbors
+  end
+
 end
--- ruby/main/game.rb
+++ ruby/main/game.rb
@@ -2,23 +2,24 @@ require './main/cell'
 require './main/board'
 
 class Game
-  attr_reader :board, :cells, :generations
-
+  attr_reader :board, :generations
+  attr_accessor :cells
   def initialize(startingCells, generations, length, height)
     @cells = startingCells # array of arrays of x,y coords [[1,3],[2,3],[0,2]]
     @generations = generations # number of iterations to run the game for
     @board = Board.new(length, height)
   end
 
-  def evolve
-    # run a single iteration of the game
-  end
-
   def start
     (1..generations).each do |i|
-      puts "========================= GENERATION #{i} =========================="
+      puts "========================== GENERATION #{i} =========================="
       board.visualize(cells)
       evolve
     end
   end
+
+  def evolve
+    self.cells = board.calculate(cells)
+  end
+
 end
